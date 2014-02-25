d972f80302d0547851ffe88c6907fe7b92062eb5
alunolp2 <aluno@email.com>
2014-02-25 15:16:46 -0300

    commit inicial da aula 01

---
+++ ThiagoLuis/aula1/aula1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros;
+
+            double mediabruta=0;
+            double medianum;
+            double medialiquida;
+            double hue;
+
+
+
+            Console.WriteLine("Digite quantos números deseja");
+            numeros=int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numeros; i++)
+            {
+                hue = i + 1;
+                Console.WriteLine("Escreva o {0}° número", hue);
+                medianum = double.Parse(Console.ReadLine());
+                mediabruta = mediabruta + medianum;
+
+                
+            }
+
+            medialiquida = (mediabruta / numeros);
+
+            Console.WriteLine("A média dos {0} números é igual a {1}", numeros, medialiquida);
+
+            Console.ReadKey();
+
+        }
+    }
+}
