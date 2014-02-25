eda273495227d772a4ce04f1497e3a89175bf8ee
alunolp2 <aluno@email.com>
2014-02-25 15:54:35 -0300

    Exercício e desafio

---
+++ FabioKumakola/aula1/aula1/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1
+{
+    class Class1
+    {
+        public int x;
+
+        public double[] n = new double[100000];
+
+        private double pmaior = 0;
+        private double pmenor = 0;
+        private double maiormedia = 0;
+        private double menormedia = 0;
+        private double soma = 0;
+        private double media;
+        private double maiorvalor = -1;
+        private double menorvalor = 11;
+        private double dp = 0;
+        private double mediana;
+
+        public string rmedia()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                soma = n[i] + soma;
+            }
+
+            this.media = Math.Round(soma / x * 100) / 100;
+
+            return "A média dos números dados são: " + media;
+        }
+
+        public string rmaiorvalor()
+        {
+            for (int i = 1; i < x; i++)
+                if (n[i] > maiorvalor)
+                    maiorvalor = n[i];
+
+            return "O maior valor é: " + maiorvalor;
+        }
+
+        public string rmenorvalor()
+        {
+            for (int i = 1; i < x; i++)
+                if (n[i] < menorvalor)
+                    menorvalor = n[i];
+
+            return "O menor valor é: " + menorvalor;
+        }
+
+        public string rmaiormedia()
+        {
+            for (int i = 0; i < x; i++)
+                if (n[i] > media)
+                    maiormedia++;
+
+            return "Tem " + maiormedia + " acima da média.";
+        }
+        public string rmenormedia()
+        {
+            for (int i = 0; i < x; i++)
+                if (n[i] < media)
+                    menormedia++;
+
+            return "Tem " + menormedia + " abaixo da média.";
+        }
+
+        public string rpmaior()
+        {
+            pmaior = Math.Round((maiormedia / x) * 100 * 100) / 100;
+            return pmaior + "% estão acima da média";
+        }
+
+        public string rpmenor()
+        {
+            pmenor = Math.Round((menormedia / x) * 100 * 100) / 100;
+            return pmenor + "% estão abaixo da média";
+        }
+
+        public string reeleicao()
+        {
+            if (media >= 4.87 && pmaior >= 60)
+                return "Será reeleito.";
+            else
+                return "Não será reeleito.";
+        }
+
+        public string rdp()
+        {
+            double[] d = new double[x];
+            
+            for (int i = 0; i < x; i++)
+                d[i] = n[i] - media;
+
+            for (int i = 0; i < x; i++)
+                d[i] = d[i] * d[i];
+
+            for (int i = 0; i < x; i++)
+                dp = dp + d[i];
+
+            dp = dp / x;
+            dp = Math.Round(Math.Sqrt(dp) * 100) / 100;
+
+            return "O desvio padrão é de: " + dp;
+        }
+
+        public string rmediana()
+        {
+            
+            if (x % 2 == 0)
+            {
+                int y = x / 2;
+                int z = y - 1;
+
+                mediana = (n[y] + n[z]) / 2; 
+            }
+            else
+            {
+                int y = x / 2;
+
+                mediana = n[y];
+            }
+            return "A mediana dos números é: " + mediana;
+        }
+    }
+
+
+}
---
+++ FabioKumakola/aula1/aula1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool erro = false;
+            do
+            {
+                try
+                {
+                    Class1 pesquisa = new Class1();
+                    Console.WriteLine("Quantidade de notas coletadas: ");
+                    pesquisa.x = int.Parse(Console.ReadLine());
+
+                    bool continuar = true;
+
+                    while (continuar == true)
+                    {
+                        if (pesquisa.x > 100000 || pesquisa.x < 0)
+                        {
+                            Console.WriteLine("Digite um número entre 0 e 100000");
+                            pesquisa.x = int.Parse(Console.ReadLine());
+                        }
+                        else
+                            continuar = false;
+                    }
+
+                    Console.WriteLine("Digite as notas(elas terão somente 2 casas decimais): ");
+
+                    bool continuar2 = true;
+
+                    while (continuar2 == true)
+                    {
+                        for (int i = 0; i < pesquisa.x; i++)
+                        {
+                            pesquisa.n[i] = Math.Round(double.Parse(Console.ReadLine()) * 100) / 100;
+
+                            if (pesquisa.n[i] > 10 || pesquisa.n[i] < 0)
+                            {
+                                i = -1;
+                                Console.WriteLine("Todos os números devem estar entre 0 e 10, redigite os números corretamente: ");
+                                for (int j = 0; j < pesquisa.x; j++)
+                                    pesquisa.n[j] = 0;
+                            }
+                            else
+                                continuar2 = false;
+
+
+                        }
+                    }
+
+                    Console.WriteLine(pesquisa.rmedia());
+                    Console.WriteLine(pesquisa.rmenorvalor());
+                    Console.WriteLine(pesquisa.rmaiorvalor());
+                    Console.WriteLine(pesquisa.rmenormedia());
+                    Console.WriteLine(pesquisa.rmaiormedia());
+                    Console.WriteLine(pesquisa.rpmenor());
+                    Console.WriteLine(pesquisa.rpmaior());
+                    Console.WriteLine(pesquisa.rdp());
+                    Console.WriteLine(pesquisa.rmediana());
+                    Console.WriteLine(pesquisa.reeleicao());
+                }
+                catch
+                {
+                    Console.WriteLine("ERRO!");
+                    Console.WriteLine("Insira novamente os dados.");
+                    erro = true;
+                }
+
+            } while (erro == true);
+	    }
+    }
+}
