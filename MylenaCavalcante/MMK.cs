979f31a5404ca19b76b3df20583381e2344acc12
alunolp2 <aluno@email.com>
2014-02-25 18:06:13 -0300

    exercicio 01 completo

--- MylenaCavalcante/MMK.cs
+++ MylenaCavalcante/MMK.cs
@@ -10,6 +10,32 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            double y;
+
+
+            double result2 = 0, result = 0, result3 = 0;
+
+            Console.WriteLine("informe a quantidade de notas que notas digitadas");
+            int x = int.Parse(Console.ReadLine());
+
+            double[] notas = new double[x];
+
+            for (int i = 0; i < x; i++)
+            {
+
+                Console.WriteLine("Digite uma das notas");
+
+                notas[i] = double.Parse(Console.ReadLine());
+
+                result = notas[i] + result;
+            }
+
+            result2 = result / x;
+
+            Console.WriteLine(result2);
+
+
+            Console.ReadKey();
         }
     }
 }
