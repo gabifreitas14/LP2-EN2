ce018d77f408976c51a5a7529d2ac287da8bf288
alunolp2 <aluno@email.com>
2014-02-25 17:42:34 -0300

    Trabalho aula 2 incrementado

--- CarlosAugusto/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ CarlosAugusto/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -10,18 +10,21 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            // Pedir quantos números ele quer
             int calculo;
-            Console.WriteLine("Quantos números deseja calcular?");
+            Console.Write("Quantos números deseja calcular? ");
             calculo = int.Parse(Console.ReadLine());
 
+            // Escrever os números que ele quer
             double[] numbers = new double[calculo];
             for (int i = 0; i < calculo; i++)
             {
-                Console.WriteLine((i + 1) + "º número:");
+                Console.Write((i + 1) + "º número: ");
                 numbers[i] = double.Parse(Console.ReadLine());
 
             }
 
+            // Descobrir a média dos números
             double soma = 0;
             int z = 0;
 
@@ -31,38 +34,72 @@ namespace ConsoleApplication2
                 z++;
             }
 
-            double media = soma/calculo;
-            Console.WriteLine("A média dos números digitados é {0}",  media);
+            double media = soma / calculo;
+            Console.WriteLine("A média dos números digitados é {0}", media);
 
-            double menor = 0; 
-            double maior = 0; 
-            
+            // Descobrir o mínimo e o máximo valor
+            double menor = 0;
+            double maior = 0;
+
 
             for (int i = 0; i < calculo; i++)
             {
                 if (i == 0)
-                 {
-                      menor = numbers[i];
-                      maior = numbers[i];
-                   }
-
-                    if (numbers[i] < menor)
-                    {
-
-                        menor = numbers[i];
-
-                    }
-                    else if (numbers[i] > maior)
-                    {
-                        maior = numbers[i];
-
-                    }
+                {
+                    menor = numbers[i];
+                    maior = numbers[i];
                 }
-            Console.WriteLine("O menor número é:" + menor);
 
-            Console.WriteLine("O maior número é:" + maior);
 
+                if (numbers[i] < menor)
+                {
+
+                    menor = numbers[i];
+
+                }
+                else if (numbers[i] > maior)
+                {
+                    maior = numbers[i];
+
+                }
             }
+            Console.WriteLine("O menor número é: " + menor);
+
+            Console.WriteLine("O maior número é: " + maior);
+            
+            // Descobrir quantos números são maiores ou menores que a média
+
+            int contador = 0;
+            int contadormenor = 0;
+            double porcento = 0;
+            double porcentomenor = 0;
+
+            for (int i = 0; i < calculo; i++)
+            {
+                if (numbers[i] >= media) {
+                    Console.WriteLine("Este número é acima da média: " + numbers[i]);
+                    contador++;
+                    }
+                else if (numbers[i] <= media)
+                {
+                    Console.WriteLine("Este número é abaixo da média: " + numbers[i]);
+                    contadormenor++;
+                }
+ }
+            Console.WriteLine(" ");
+            Console.WriteLine("Existem {0} números maiores que a média.", contador);
+            Console.WriteLine("Existem {0} números menores que a média", contadormenor);
+            porcento = (contador * 100) / calculo;
+            porcentomenor = (contadormenor * 100) / calculo;
+
+            Console.WriteLine("A porcentagem de números maiores que a média é: {0}%", porcento);
+            Console.WriteLine("A porcentagem de números menores que a média é: {0}%", porcentomenor);
+
+
+            
+            
+        }
     }
 }
+
 
