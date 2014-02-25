4f26556b9f25bc7cb0da67695b06938c455d89fb
Aluno LP2 <alunolp2@email .com>
2014-02-25 17:47:31 -0300

    cálculo das médias

--- LuccasZagallo/Program.cs
+++ LuccasZagallo/Program.cs
@@ -10,7 +10,38 @@ namespace testeGit
     {
         static void Main(string[] args)
         {
+            double result = 0, result2 = 0, soma = 0, media;
+            double notamax;
+            int contador = 0;
+            double notamin;
 
-        }
+            Console.WriteLine("Digite a quantidade de notas");
+            int qtd = int.Parse(Console.ReadLine());
+
+            double[] notas = new double[qtd];
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine("Digite a nota");
+                notas[i] = int.Parse(Console.ReadLine());
+            }
+     
+            foreach (var nota in notas)
+            {
+                if(contador < qtd)
+                {
+                    soma += notas[contador];
+                    contador++;
+                }
+            }
+
+            media = soma / qtd;
+            Console.WriteLine("A média é: " + media);
+
+            
+       
+            Console.ReadKey();
+
+        } 
     }
 }
