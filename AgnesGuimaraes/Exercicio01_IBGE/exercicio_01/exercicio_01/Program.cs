89b2b7461e56c768d51adae59d107b8b94765acd
alunolp2 <aluno@email.com>
2014-02-25 15:18:07 -0300

    commit inicial da aula 1

---
+++ AgnesGuimaraes/Exercicio01_IBGE/exercicio_01/exercicio_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            double media = 0;
+                        
+            Console.WriteLine("Digite a quantidade de notas coletadas na pesquisa do IBGE: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] g = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite a nota coletada: ");
+                g[i] = double.Parse(Console.ReadLine());
+                soma += g[i];
+            }
+
+            // a) Média da avaliação do governo federal.
+            Convert.ToDouble(n);
+            media = soma / n;
+            Console.WriteLine("A média de avaliação do Governo Federal é de " + media);
+
+            // b) Menor nota dada...
+            
+        }
+    }
+}
---
+++ AgnesGuimaraes/Exercicio01_IBGE/exercicio_01/exercicio_01/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("exercicio_01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("exercicio_01")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62cfb0fe-9792-4b68-ae15-a665c1c0ba71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
