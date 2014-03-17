db361e8ab539f6fc9ba5aa64529e8f07661414f9
Gabriela <gabriela-_-freitas@hotmail.com>
2014-03-17 18:15:46 -0300

    Desafio 1

--- GabrielaBarbosa/Aula 2/Program.cs
+++ GabrielaBarbosa/Aula 2/Program.cs
@@ -11,47 +11,90 @@ namespace Aula_2
         static void Main(string[] args)
         {
              double soma = 0;
+
+            
+
+             string materia = Console.ReadLine();
+
+             string professor = Console.ReadLine();
  
              int qtdaluno=int.Parse(Console.ReadLine());
  
              int qtdavaliacao=int.Parse(Console.ReadLine());
  
-             int [,] alunonotas = new int[qtdaluno,qtdavaliacao];
+             double [,] alunonotas = new double[qtdaluno,qtdavaliacao];
  
              string [] nomealuno= new string[qtdaluno];
 
              double [] media = new double [qtdaluno];
- 
-             for (int i = 0; i < qtdaluno; i++)
+
+             if (qtdaluno >= 2 && qtdaluno <= 1000 && qtdavaliacao >= 2 && qtdavaliacao <= 8)
              {
-                 nomealuno[i] = Console.ReadLine();
- 
-                 for (int a = 0; a < qtdavaliacao; a++)
+
+
+                 for (int i = 0; i < qtdaluno; i++)
                  {
-                     alunonotas[i, a] = int.Parse(Console.ReadLine());
+                     nomealuno[i] = Console.ReadLine();
 
-                     soma += alunonotas[i, a];
 
+
+                     for (int a = 0; a < qtdavaliacao; a++)
+                     {
+                         alunonotas[i, a] = double.Parse(Console.ReadLine());
+
+                         soma += alunonotas[i, a];
+
+                     }
+
+                     media[i] = soma / qtdavaliacao;
+
+                     soma = 0;
                  }
 
-                 media[i] = soma / qtdavaliacao;
+                 Console.WriteLine("Consolidação de Médias \r\nMatéria: {0} \r\nProfessor: {1} \r\n",materia,professor);
 
-                 soma = 0;
+                     
 
-                
-                    
- 
-                 } 
             
             for (int k = 0; k < qtdaluno; k++)
 
                  {
-                     Console.WriteLine(nomealuno[k]+" : ");
 
-                     Console.WriteLine(media[k]);
+                     if (media[k]>=6)
+                     {
+                         Console.WriteLine(string.Format("{0,-20} : {1:F1} (Aprovado)", nomealuno[k], media[k]));
+                     }
+
+                     else if (media[k]<6 && media[k]>=5.5)
+                     {
+                         Console.WriteLine(string.Format("{0,-20} : {1:F1} (Pendente COC)", nomealuno[k], media[k]));
+                     }
+
+                     else
+                     {
+                         Console.WriteLine(string.Format("{0,-20} : {1:F1} (Reprovado)", nomealuno[k], media[k]));
+                     }
+
+                    
+
+                     
  
                 
              }
+
+  
+             }
+
+             else
+             {
+                 Console.WriteLine("Voce digitou algo errado");
+             }
+
+             
+
+            
+
+            
         }
     }
 }
