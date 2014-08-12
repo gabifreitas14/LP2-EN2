15481af0f575be5f61f39a86be56fd3a00e79381
Gabriela <gabriela-_-freitas@hotmail.com>
2014-08-11 22:19:32 -0300

    CPF no Forms

---
+++ GabrielaBarbosa/TransformandoParaForms/TransformandoParaForms/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TransformandoParaForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCPF = new System.Windows.Forms.TextBox();
+            this.labelCPF = new System.Windows.Forms.Label();
+            this.buttonVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCPF
+            // 
+            this.textBoxCPF.Location = new System.Drawing.Point(25, 71);
+            this.textBoxCPF.Name = "textBoxCPF";
+            this.textBoxCPF.Size = new System.Drawing.Size(207, 20);
+            this.textBoxCPF.TabIndex = 0;
+            // 
+            // labelCPF
+            // 
+            this.labelCPF.AutoSize = true;
+            this.labelCPF.Location = new System.Drawing.Point(90, 35);
+            this.labelCPF.Name = "labelCPF";
+            this.labelCPF.Size = new System.Drawing.Size(69, 13);
+            this.labelCPF.TabIndex = 1;
+            this.labelCPF.Text = "Digite o CPF:";
+            // 
+            // buttonVerificar
+            // 
+            this.buttonVerificar.Location = new System.Drawing.Point(93, 137);
+            this.buttonVerificar.Name = "buttonVerificar";
+            this.buttonVerificar.Size = new System.Drawing.Size(75, 23);
+            this.buttonVerificar.TabIndex = 2;
+            this.buttonVerificar.Text = "Verificar";
+            this.buttonVerificar.UseVisualStyleBackColor = true;
+            this.buttonVerificar.Click += new System.EventHandler(this.buttonVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 203);
+            this.Controls.Add(this.buttonVerificar);
+            this.Controls.Add(this.labelCPF);
+            this.Controls.Add(this.textBoxCPF);
+            this.Name = "Form1";
+            this.Text = "Validador de CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCPF;
+        private System.Windows.Forms.Label labelCPF;
+        private System.Windows.Forms.Button buttonVerificar;
+    }
+}
+
---
+++ GabrielaBarbosa/TransformandoParaForms/TransformandoParaForms/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransformandoParaForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonVerificar_Click(object sender, EventArgs e)
+        {
+            int[] cpf = new int[11];
+
+            int[] um = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 0 };
+
+            int[] dois = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            int[] numeros = new int[10];
+
+            int[] numerais = new int[10];
+
+            int soma1 = 0;
+
+            int soma2 = 0;
+
+            int i = 0;
+            foreach (char digito in textBoxCPF.Text)
+            {
+                cpf[i++] = int.Parse(digito.ToString());
+
+            }
+
+           
+
+
+
+               
+
+                for (int a = 0; a < 10; a++)
+                {
+                    numeros[a] = um[a] * cpf[a];
+
+                    numerais[a] = dois[a] * cpf[a];
+
+                    soma1 = soma1 + numeros[a];
+
+                    soma2 = soma2 + numerais[a];
+
+                }
+
+
+                int divisao = soma1 % 11;
+
+                int divisoes = soma2 % 11;
+
+                if (divisao == 0 || divisao == 1)
+                {
+                    if (cpf[9] != 0)
+                    {
+                        MessageBox.Show("CPF Inválido");
+
+                    }
+
+
+                }
+
+
+                else if ((11 - divisao) != cpf[9])
+                {
+
+                    MessageBox.Show("CPF Inválido");
+
+
+                }
+
+                else if (divisoes == 0 || divisoes == 1)
+                {
+
+                    if (cpf[10] != 0)
+                    {
+                        MessageBox.Show("CPF Inválido");
+
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("CPF Válido");
+                    }
+
+                }
+
+                else
+                {
+
+                    if (11 - divisoes != cpf[10])
+                    {
+                       MessageBox.Show("CPF Inválido");
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("CPF Válido");
+                    }
+
+
+                }
+
+                textBoxCPF.Focus();
+
+            }
+
+        }
+    }
