f2e3401731ca709ab40c6ca63d290240c25dc2b5
Gabriela <gabriela-_-freitas@hotmail.com>
2014-08-11 22:24:25 -0300

    Não terminei pois não estou entendendo

---
+++ GabrielaBarbosa/Contatos/People.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[ContactID] INT NOT NULL IDENTITY, 
+    [LastCall] DATETIME NULL, 
+    [Name] NVARCHAR(50) NULL, 
+    [Client] BIT NULL, 
+    [Company] NVARCHAR(50) NULL, 
+    [Telephone] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([ContactID]) 
+)
